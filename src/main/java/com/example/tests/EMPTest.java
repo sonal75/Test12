a12ec682d747e19ae958ae027265aecac29847ed
sonal75 <sonal.sikligar@gmail.com>
2019-08-14 15:07:38 -0500

    Merge origin/master

---
+++ src/main/java/com/example/tests/EMPTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.tests;
+
+/**
+ *
+ * @author silks
+ */
+public class EMPTest {
+    public class Calculator {
+    public int add(int a, int b){
+        if (a>100){
+            return a+b;
+        }
+    return a+b;
+}
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+     //public static void main(String[] args) {
+        // TODO code application logic here
+        //Calculator c1=new Calculator();
+        //System.out.println(c1.add(6,4));
+        
+    }
+    
+}
+
+    
+
